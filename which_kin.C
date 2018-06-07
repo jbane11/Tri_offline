6f850dcab560d7f936faf85b130a860b2233725d
jban11 <jbane11@gmail.com>
2018-06-07 12:06:29 -0400

    which kin?

--- which_kin.C
+++ which_kin.C
@@ -1,22 +1,36 @@
 
 
-void which_kin(int run){
+int which_kin(int run){
 
 	ifstream input ("total_runlist.dat");
 
 	string line;
 
 	vector<string> tmp;
-	int in_run=0;
+	int in_run=-10;
 	int in_kin=0;
-	while(input.getline(line);){
+	int real_kin=-1;
+	while(getline(input,line)){
 		int pos;
-		while(pos=line.find("\t") !=string::npos){
-			tmp.push_back(line.substr(0,pos));
+		
+		pos=line.find("\t");
+		tmp.push_back(line.substr(0,pos));
+		pos=line.find_last_of("_");
+		tmp.push_back(line.substr(pos+1,line.size()));
+		in_run=stoi(tmp[0]);
+		in_kin=stoi(tmp[1]);
+			
+		tmp.clear();
+
+		if(run==in_run){
+			cout<< " kin =" <<in_kin<<endl;
+			real_kin=in_kin;
+			break;}
 		}
-		cout << tmp[0]<<" " <<tmp[1] <<endl;	
-	}
-}	
+		
+return real_kin;
+}
+	
 		
 		
 		
