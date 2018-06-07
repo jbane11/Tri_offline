1d9803710afd1c04bef29c65c42f3d7c84b2427e
Jason Bane <jbane11@github.com>
2018-06-07 14:27:06 -0400

    batch_ped

---
+++ ped_scan/batch_ped_scan.C
@@ -0,0 +1,14 @@
+//////////////////////////////////////
+// This script will be used to run 
+// the ped scan script in the batch system.
+// This script will run the already compiled
+// version of ped scan. This script will need
+// the run number!!
+/////////////////////////////////////////
+
+
+void batch_ped_scan(int run){
+
+	gROOT->LoadMacro("ped_scan_C.so");
+	gROOT->ProcessLine(Form("ped_scan(%d)",run));
+}
