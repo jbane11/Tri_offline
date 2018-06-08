53ab9797313f9c3528a12bec7921cf6334b76a5b
Jason Bane <jbane11@github.com>
2018-06-08 14:18:43 -0400

    headers

--- headers/TRI_Main.h
+++ headers/TRI_Main.h
@@ -68,7 +68,7 @@
 using namespace std;
 
 /*Customization{{{*/
-TString ROOTFILES_DIR = "/v/lustre2/expphy/volatile/halla/triton/Marathon_Rootfiles/pass1/";
+TString ROOTFILES_DIR = "/volatile/halla/triton/Marathon_Rootfiles/pass1/";
 //TString ROOTFILES_DIR = "/volatile/halla/triton/Tritium_Rootfiles/";
 //TString ROOTFILES_DIR = "/chafs1/work1/tritium/pass1/";
 //TString ROOTFILES_DIR = "./Rootfiles/";
