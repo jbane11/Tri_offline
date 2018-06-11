3401ddd7e59e9431841d58872233f301fc4eef36
jban11 <jbane11@gmail.com>
2018-06-11 10:45:55 -0400

    updates

---
+++ ped_scan/get_root.sh
@@ -0,0 +1,2 @@
+jcache get /mss/halla/triton/prod/pass1_calibration//kin-1/tritium_1121*.root -e jbane@jlab.org
+jcache get /mss/halla/triton/prod/pass1_calibration//kin-1/tritium_1122*.root -e jbane@jlab.org
