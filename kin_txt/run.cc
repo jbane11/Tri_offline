c81d075738712fd249dd62b25c90988d031589a2
Jason Bane <jbane11@github.com>
2018-06-27 16:16:26 -0400

    Bugs in corrections table

--- kin_txt/run.cc
+++ kin_txt/run.cc
@@ -2,7 +2,7 @@
 
 void run(TString filename ="", int ow = 1, int debug =0)
 {
-
+	gROOT->LoadMacro("Corrections_table_C.so");
 	gROOT->ProcessLine(Form("Corrections_table(\"%s\",%d,%d)",filename.Data(),ow,debug));
 
 }
