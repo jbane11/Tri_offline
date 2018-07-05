6d478e27ccd549dae1dece8344429a35c8ca89e4
Jason Bane <jbane11@github.com>
2018-07-05 10:56:09 -0400

    ped_scan

--- ped_scan/ped_scan.C
+++ ped_scan/ped_scan.C
@@ -5,22 +5,22 @@
 
 
 void ped_scan( int run = 0 , int debug=0, int single_det=-1){
-	if(run==0){cout << "Please Enter run number" <<endl; cin >> run;}
-	if(debug) cout << "Debug is turn on: if you supply a run number this will be the first statment printed!! "<<endl<<endl;
+	if(run==0){cout << "Please Enter run number" <<"\n"; cin >> run;}
+	if(debug) cout << "Debug is turn on: if you supply a run number this will be the first statment printed!! "<<"\n";
 	gStyle->SetOptStat(0);
 	std::vector< string> tmp_v(34);
 	std::vector< std::vector<string>> ped_scan(7,tmp_v);
 	ped_scan.begin();
-cout << ped_scan.size() <<endl;
+cout << ped_scan.size() <<"\n";
 
 	for(unsigned int i=0; i<ped_scan.size();i++){
 		for(unsigned int j=0; j<ped_scan[i].size();j++){
 			cout<<ped_scan[i][j] << " ,";}
-		cout<<endl;
+		cout<<"\n";
 	}
 	ped_scan.clear();
 
-//	ped_scan.reserve(7);
+//	ped_scan.reserve();
 	string image = "./det";
 	//Correct arm varribles
 	string Arm="",ARM="",arm="";
@@ -39,7 +39,7 @@ cout << ped_scan.size() <<endl;
 	TChain *Temp = LoadCalib(run);
 	if(Temp==nullptr)
 	{
-		cout << "Error on first run: run number ("<< first_run<<"). Exiting!!"<<endl;
+		cout << "Error on first run: run number ("<< first_run<<"). Exiting!!"<<"\n";
 		exit(0);
 	}
 	Temp->SetBranchStatus("*",0);
@@ -47,12 +47,12 @@ cout << ped_scan.size() <<endl;
 
 	ofstream output(Form("./ped_table/%d.csv",run));
 	if(!output.is_open()){
-		cout<<"Ped table not open!"<<endl;
+		cout<<"Ped table not open!"<<"\n";;
 		exit(0);}	
 	//make a buffer for the output
 	
 	char mybuffer [512];
-	output.rdbuf()->pubsetbuf(mybuffer,512);	
+	output.rdbuf()->pubsetbuf(mybuffer,2048);	
 	//Find the correct name for the detector to ploti
 	string det_array[] ={".s0",".s2",".prl1",".prl2",".cer",".sh",".ps"};
 	string suffix_array[] = {".a",".la",".ra"};
@@ -89,23 +89,30 @@ cout << ped_scan.size() <<endl;
 				nod++;
 			}
 			if(debug)cout<<"  \t"<< tbn;
-			tbn=0;
 			TB =nullptr; 
 			TBN=nullptr;
 			if(tbn>max_channel)max_channel=tbn;
 			
-			if(debug)cout<<endl;
+			tbn=0;
+			if(debug)cout<<"\n";
 		}//end of suffix loop
 	}//end of prefix loop
 	///////////////////////
 	////////	
 
-	if(debug)cout<< "Found " << detector.size() << " applical(sp) dets!" <<endl;
-	if(debug)cout<< "The maximum number of channels is " <<max_channel<<endl;
+	if(debug)cout<< "Found " << detector.size() << " applical(sp) dets!" <<"\n";
+	if(debug)cout<< "The maximum number of channels is " <<max_channel<<"\n";
 
 
 	vector <string> oneD; oneD.push_back("Detector/PMT#");
-	for(int j=0;j<max_channel;j++){oneD.push_back(to_string(j));}
+	for(int j=0;j<max_channel;j++)
+	{
+		oneD.push_back(to_string(j));
+		oneD.push_back(Form("Sigma_%d",j));
+		if(debug) cout << oneD[j*2] <<",";
+	}
+	if(debug)cout<<oneD[oneD.size()-1];
+	if(debug)cout<<"\n";
 	ped_scan.push_back(oneD);
 	oneD.clear();
 	
@@ -122,7 +129,7 @@ cout << ped_scan.size() <<endl;
 	for(unsigned int dets=detss;dets<detector.size();dets++)
 	{
 	
-if(debug)cout<<"Debug: start of dets loop:  " << dets <<endl;
+if(debug)cout<<"Debug: start of dets loop:  " << dets <<"\n";
 		//make A canvas with the closest but sligtly more 
 		//or = num of pads. 
 		int x_d = floor(sqrt(num_of_channels[dets]));
@@ -131,7 +138,7 @@ if(debug)cout<<"Debug: start of dets loop:  " << dets <<endl;
 		int num_of_Cs = ceil(num_of_channels[dets]/12.0);
 		TCanvas *C[num_of_Cs];
 		int C_select=0;
-		if(debug)cout <<"There will be " << num_of_Cs<< " canvases!"<<endl;
+		if(debug)cout <<"There will be " << num_of_Cs<< " canvases!"<<"\n";
 
 		for(int noc =0;noc<num_of_Cs;noc++)
 		{
@@ -148,7 +155,7 @@ if(debug)cout<<"Debug: start of dets loop:  " << dets <<endl;
 				C[noc]->Divide(x_d,y_d);
 				if(debug) cout << "is"<< x_d<< " x " <<y_d<< "  ";
 			}
-			if(debug)cout << "there are "<< (num_of_channels[dets]-(noc*12)) <<" left"<< endl;
+			if(debug)cout << "there are "<< (num_of_channels[dets]-(noc*12)) <<" left"<< "\n";
 		
 		}
 		oneD.push_back(Form("%s",detector[dets].c_str()));
@@ -161,19 +168,19 @@ if(debug)cout<<"Debug: start of dets loop:  " << dets <<endl;
 		TLegend *leg[num_of_channels[dets]];
 		//Loop through each PMT of the detectro
 		int pad=1; //pad for drawing
-
-if(debug)cout<<"Debug: start of pmt loop:  " <<endl;
-		for(int pmt=0; pmt<num_of_channels[dets];pmt++)
+		int pmt=0;
+if(debug)cout<<"Debug: start of pmt loop:  " <<"\n";
+		for(pmt=0; pmt<num_of_channels[dets];pmt++)
 		{
 
-			if(pmt/12.0 == pmt/12&&pmt>>0){C_select++;pad=1; if(debug){cout << "Next canvas" <<endl;}}
+			if(pmt/12.0 == pmt/12&&pmt>>0){C_select++;pad=1; if(debug){cout << "Next canvas" <<"\n";}}
 			if(debug)cout<< "pmt "<<pmt<<" "; 
 			//Make a hist for each pmt, need to keep till end of loop 
 			//so I can add this canvas to a PDF.
 			hist[pmt] = new TH1F(Form("hist%d",pmt),
 				Form("%s[%d]",detector[dets].c_str(),pmt)
 				,10000,0,50000);
-			if(debug) cout<< "Debug: intialize hist" <<endl;	
+			if(debug) cout<< "Debug: intialize hist" <<"\n";	
 			
 			C[C_select]->cd(pad);
 			//Draw the branch.
@@ -218,7 +225,7 @@ if(debug)cout<<"Debug: start of pmt loop:  " <<endl;
 			int counter=0;
 			double red_x_F1 = F1[pmt]->GetChisquare()/(F1[pmt]->GetNDF()*1.0);
 			double old_red =red_x_F1;
-			if(debug)cout <<"\t"<< F1[pmt]->GetParameter(1)<<" " <<red_x_F1<<" "<<endl;
+			if(debug)cout <<"\t"<< F1[pmt]->GetParameter(1)<<" " <<red_x_F1<<" "<<"\n";
 			while(red_x_F1>7.00)
 			{
 				counter++;
@@ -229,7 +236,7 @@ if(debug)cout<<"Debug: start of pmt loop:  " <<endl;
 						min_fit,max_fit);
 				red_x_F1 = F1[pmt]->GetChisquare()/(F1[pmt]->GetNDF()*1.0);
 				if(debug)cout<<" Refit " << F1[pmt]->GetParameter(1)
-					     <<" " <<red_x_F1<<" " <<max_fit<<" "<<endl;
+					     <<" " <<red_x_F1<<" " <<max_fit<<" "<<"\n";
 				if(old_red<=red_x_F1){
 					if(counter==1){
 						//F1[pmt]->SetParLimits(1,peak_1*0.95,peak_1*1.05);
@@ -265,16 +272,16 @@ if(debug)cout<<"Debug: start of pmt loop:  " <<endl;
 			double red_x_F2 = F2[pmt]->GetChisquare()/(F2[pmt]->GetNDF()*1.0);
 			red_x_F1 = F1[pmt]->GetChisquare()/(F1[pmt]->GetNDF()*1.0);
 	
-			if(debug){cout <<endl;
+			if(debug){cout <<"\n";
 				cout <<"\t"<< F1[pmt]->GetParameter(1)<<" " <<red_x_F1<<" ";
 				cout << "\t"<< F2[pmt]->GetParameter(1)<<" " <<red_x_F2<<" ";
-				cout << peaks[0]<<" "<<peaks[1] <<endl;
+				cout << peaks[0]<<" "<<peaks[1] <<"\n";
 			}
 			
 		
 			oneD.push_back(to_string(F1[pmt]->GetParameter(1)));
-	
-			if(debug)cout<<endl;
+			oneD.push_back(to_string(F1[pmt]->GetParameter(2)));	
+			if(debug)cout<<"\n";
 			//////////////////////////////////
 			//write the ped and draw legend//
 			//
@@ -290,21 +297,37 @@ if(debug)cout<<"Debug: start of pmt loop:  " <<endl;
 			pad++; //pad for drawing on canvas
 			peaks=nullptr;
 		}//End of PMT loop
+	
+		int new_z=0;
+		while(pmt<34){
+			oneD.push_back("0.0");
+			oneD.push_back("0.0");
+			pmt++;
+			new_z++;
+		}
 
-		if(debug)cout<<" End of PMT loop "<<dets<<endl;	
+		if(debug)cout<< "Added in " <<new_z <<" zeros for python script!!" <<"\n";						
+
+		if(debug)cout<<" End of PMT loop "<<dets<<"\n";	
 		ped_scan.push_back(oneD);
-		if(debug)cout << "Push back "<< oneD[0]<<" "<< ped_scan[dets+1][0]<<endl;
+		if(debug)cout << "Push back "<< oneD[0]<<" "<< ped_scan[dets+1][0]<<"\n";
 		
 		oneD.clear();
-		if(debug)cout<<"Cleared the oneD vector"<<endl;
+		if(debug)cout<<"Cleared the oneD vector"<<"\n";
 
 		//Add the canvas to a pdf
 		for(int noc=0;noc<num_of_Cs;noc++){
 			string opt="";
 			if(noc==0&&num_of_Cs>1)opt="(";
 			if(noc==(num_of_Cs-1)&&noc!=0)opt=")";
-			C[noc]->Print(Form("%s/Pedestals_%s_%d.pdf%s",image.c_str(),
-						detector[dets].c_str(),run,opt.c_str()));
+			//Add the run number to the canvas
+			C[noc]->cd();
+			TLatex t(0.2,0.95,Form("Run - %d", run));
+			t.SetNDC(kTRUE);
+			t.Draw(); 
+
+		//	C[noc]->Print(Form("%s/Pedestals_%s_%d.pdf%s",image.c_str(),
+		//				detector[dets].c_str(),run,opt.c_str()));
 		}
 		
 		if(single_det>=0)break;
@@ -325,8 +348,8 @@ if(debug)cout<<"Debug: start of pmt loop:  " <<endl;
 			output<<ped_scan[k][m]<<",";
 			if(debug)cout<<ped_scan[k][m]<<",";
 			}
-		output<<endl;
-		if(debug)cout<<endl;
+		output<<"\n";
+		if(debug)cout<<"\n";
 	}
 
 	output.close();
